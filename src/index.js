63d0cb1d1200bc0ccc8b102980516aa1563f4790
Vita Kostiuk <vitagrebennik@gmail.com>
2022-02-09 13:17:13 +0200

    add markup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,58 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  input: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+}
+
+// const BASE_URL = 'https://restcountries.com/v3.1';
+  
+refs.input.addEventListener('input', getCountryByName);
+
+function getCountryByName(e) {
+  const countryName = e.currentTarget.value;
+  // console.log(countryName);
+  fetch(`https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages`)
+    .then(response => response.json())
+    .then(countries => {
+      const markupList = createMarkup(countries);
+      refs.countryList.innerHTML = markupList;
+      
+      if (countries.length === 1) {
+        const markupInfo = createMarkupInfo(countries[0]);
+        refs.countryInfo.innerHTML = markupInfo;
+      }
+    });
+};
+
+
+function createMarkupInfo(country) {
+  const languagesValues = Object.values(country.languages).join(", ");
+  return `
+    <span class="country-item">
+      <img class="country-logo" src="${country.flags.svg}" alt="flag-icon" width="25">
+      <h1 class="country-name info">${country.name.common}</h1>
+    </span>
+    <ul class="country-list">
+    <li class="country-item">
+      <span class="country-item title">Capital: &nbsp;</span>${country.capital}</li>
+    <li class="country-item">
+      <span class="country-item title">Population: &nbsp;</span>${country.population}</li>
+    <li class="country-item">
+      <span class="country-item title">Languages: &nbsp;</span>${languagesValues}</li>
+    </ul>
+  `
+};
+
+function createMarkup(countries) {
+      const markupList = countries.map(country => `
+      <li class="country-item">
+        <img class="country-logo" src="${country.flags.svg}" alt="flag-icon" width="25">
+        <p class="country-name">${country.name.official}</p>
+      </li>`).join('');
+
+      return markupList;
+};
---
+++ src/services/api-restcountries.js
@@ -0,0 +1,10 @@
+// const BASE_URL = 'https://restcountries.com/v3.1/name';
+
+// function fetchCountry(name) {
+//   return fetch(`${BASE_URL}/${name}`)
+//     .then(response => {
+//       return response.json();
+//   })
+// }
+
+// export default fetchCountry;
